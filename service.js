9713e71e0c834536ecded773a5af5a685d8ab7e7
AokumaPRO <241301857+AokumaPRO@users.noreply.github.com>
2026-02-06 23:07:47 +0900

    service.jsを作成

---
+++ service.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded',()=>{
+  const cards = document.querySelectorAll('.business-card');
+
+  const observer=new IntersectionObserver((entries)=>{
+    entries.forEach(entry =>{
+      if (entry.isIntersecting){
+        entry.target.classList.add('is-show');
+      }
+    });
+  }, {
+    threshold:0.2
+  });
+  cards.forEach(card =>observer.observe(card));
+});
